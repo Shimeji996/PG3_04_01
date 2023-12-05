a6bebd1499690b14f5e320473c0d5552369c6234
Shimeji996 <k022g0021@g.neec.ac.jp>
2023-12-05 17:15:13 +0900

    プロジェクト ファイルを追加します。

---
+++ InputHandler.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Command.h"
+#include <Novice.h>
+
+class InputHandler
+{
+public:
+	ICommand* HandleInput();
+
+	void AssignMoveRightCommand2PressKeyD();
+	void AssignMoveLeftCommand2PressKeyA();
+
+private:
+
+	ICommand* pressKeyA;
+	ICommand* pressKeyD;
+};
+
---
+++ Player.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Vector2.h"
+#include <Novice.h>
+
+class Player
+{
+private:
+
+	Vector2 pos = { 640.0f,360.0f };
+	float speed = 4.0f;
+
+public:
+	Player();
+
+	void Initialize();
+	void Update();
+	void Draw();
+
+	void MoveRight();
+	void MoveLeft();
+};
+
